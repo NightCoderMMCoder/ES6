a62d54b67734b22b56f0e2ca5e71c2d872fbe1be
aungmyat12 <aungmyatoo2580@gmail.com>
2021-06-16 01:56:09 +0630

    2-TemplateLiterals

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var name = "Aung Myat Oo";
+var name = `Aung Myat Oo`;
 var programmer = true; // global
 let message;
+let age = 30;
 
 if (programmer) {
-  message = name + " is a programmer"; // block scope
+  message = `${name} is a programmer and ${age} years old`; // block scope
 }
 console.log(message);
 
